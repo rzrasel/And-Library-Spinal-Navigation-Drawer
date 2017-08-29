a1e3808a67a6c8b740f5f5d4adfd97c247a64894
Developer <developer@Shurjo.local>
2017-08-29 16:13:13 +0600

    Use and create example

--- andlibraryspinalnavigationdrawer/src/main/java/com/rz/spinalnavdrawer/SpinalNavDrawer.java
+++ andlibraryspinalnavigationdrawer/src/main/java/com/rz/spinalnavdrawer/SpinalNavDrawer.java
@@ -13,7 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by Rz Rasel on 2017-08-28.
@@ -24,14 +27,12 @@ public class SpinalNavDrawer {
     private Activity activity;
     private Context context;
     //|------------------------------------------------------------|
-    private Toolbar sysToolBar;
+    private Toolbar spinalToolBar;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-    private DrawerLayout sysDrawerLayout;
-    private ListView sysDrawerList;
-
-    //|------------------------------------------------------------|
-    public SpinalNavDrawer() {
-    }
+    private DrawerLayout spinalDrawerLayout;
+    private RelativeLayout spinalDrawerContainerLayout;
+    private ListView spinalDrawerContainerView;
+    private ListView spinalDrawerList;
 
     //|------------------------------------------------------------|
     public SpinalNavDrawer(Activity argActivity, Context argContext) {
@@ -40,92 +41,164 @@ public class SpinalNavDrawer {
     }
 
     //|------------------------------------------------------------|
-    @Deprecated
-    public void onInitActivity(Activity argActivity, Context argContext, Toolbar argSysToolBar) {
-        this.activity = argActivity;
-        this.context = argContext;
-        this.sysToolBar = argSysToolBar;
-        onConfigureToolBar(sysToolBar);
-    }
-
-    //@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-    }
-
-    public SpinalNavDrawer onConfigureToolBar(Toolbar argSysToolBar) {
-        sysToolBar = argSysToolBar;
-        /*ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
-            actionBar.hide();
-        if (argSysToolBar != null) {
-            setSupportActionBar(argSysToolBar);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }*/
-        //|------------------------------------------------------------ |
-        ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
-        if (actionBar != null)
-            actionBar.hide();
-        ((AppCompatActivity) activity).setSupportActionBar(sysToolBar);
-        sysToolBar.setTitleTextColor(Color.parseColor("#ffffff"));
-        ((AppCompatActivity) activity).getSupportActionBar().setHomeButtonEnabled(true);
-        ((AppCompatActivity) activity).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //((AppCompatActivity) activity).getSupportActionBar().setElevation(0);
-        ((AppCompatActivity) activity).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#80000000")));
-        //|------------------------------------------------------------ |
-        /*ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
-        if (actionBar != null)
-            actionBar.hide();
-        sysToolBar = argSysToolBar;
-        //sysToolBar = (Toolbar) findViewById(R.id.sysToolBar);
-        //sysToolBar.setTitleTextColor(Color.parseColor("#ffffff"));
-        setSupportActionBar(sysToolBar);*/
-        /*((AppCompatActivity) activity).getSupportActionBar().setHomeButtonEnabled(true);
-        ((AppCompatActivity) activity).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ((AppCompatActivity) activity).getSupportActionBar().setElevation(0);
-        ((AppCompatActivity) activity).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#80000000")));*/
-        //|------------------------------------------------------------|
+    public SpinalNavDrawer setToolBar(Toolbar argSpinalToolBar) {
+        spinalToolBar = argSpinalToolBar;
         return this;
     }
 
-    public SpinalNavDrawer onConfigureDrawer(DrawerLayout argSysDrawerLayout) {
-        //|------------------------------------------------------------|
-        sysDrawerLayout = argSysDrawerLayout;
-        //|------------------------------------------------------------|
-        actionBarDrawerToggle = new ActionBarDrawerToggle(activity, argSysDrawerLayout, com.rz.spinalnavdrawer.R.string.spinal_nav_drawer_open, com.rz.spinalnavdrawer.R.string.spinal_nav_drawer_close) {
-            /* Called when drawer is closed */
-            public void onDrawerClosed(View view) {
-                //Put your code here
-                activity.invalidateOptionsMenu();
-            }
+    public SpinalNavDrawer setDrawerLayout(DrawerLayout argSpinalDrawerLayout) {
+        spinalDrawerLayout = argSpinalDrawerLayout;
+        return this;
+    }
 
-            /* Called when a drawer is opened */
-            public void onDrawerOpened(View drawerView) {
-                //Put your code here
-                activity.invalidateOptionsMenu();
-            }
-        };
-        /*sysToolBar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sysDrawerLayout.openDrawer(GravityCompat.START);
-            }
-        });*/
-        actionBarDrawerToggle.syncState();
-        sysDrawerLayout.addDrawerListener(actionBarDrawerToggle);
-        //|------------------------------------------------------------|
+    public SpinalNavDrawer setDrawerContainerLayout(RelativeLayout argSpinalDrawerContainerLayout) {
+        spinalDrawerContainerLayout = argSpinalDrawerContainerLayout;
+        return this;
+    }
+
+    public SpinalNavDrawer setDrawerContainerLayout(ListView argSpinalDrawerContainerView) {
+        spinalDrawerContainerView = argSpinalDrawerContainerView;
+        return this;
+    }
+
+    public SpinalNavDrawer setDrawerListView(ListView argSpinalDrawerList) {
+        spinalDrawerList = argSpinalDrawerList;
         return this;
     }
 
     //|------------------------------------------------------------|
-    public void onPostCreate() {
-        actionBarDrawerToggle.syncState();
-    }
-
-    public boolean onOptionsItemSelected(MenuItem argMenuItem) {
-        if (actionBarDrawerToggle.onOptionsItemSelected(argMenuItem)) {
-            return true;
+    public class OnSetupSpinalToolBar {
+        public OnSetupSpinalToolBar() {
+            //|------------------------------------------------------------ |
+            /*ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null)
+                actionBar.hide();
+            if (argSysToolBar != null) {
+                setSupportActionBar(argSysToolBar);
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            }*/
+            //|------------------------------------------------------------ |
+            /*ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
+            if (actionBar != null)
+                actionBar.hide();
+            sysToolBar = argSysToolBar;
+            //sysToolBar = (Toolbar) findViewById(R.id.sysToolBar);
+            //sysToolBar.setTitleTextColor(Color.parseColor("#ffffff"));
+            setSupportActionBar(sysToolBar);*/
+            /*((AppCompatActivity) activity).getSupportActionBar().setHomeButtonEnabled(true);
+            ((AppCompatActivity) activity).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            ((AppCompatActivity) activity).getSupportActionBar().setElevation(0);
+            ((AppCompatActivity) activity).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#80000000")));*/
+            //|------------------------------------------------------------|
         }
+
+        public OnSetupSpinalToolBar onHideActionBar() {
+            ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
+            if (actionBar != null)
+                actionBar.hide();
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onSetActionBar() {
+            ((AppCompatActivity) activity).setSupportActionBar(spinalToolBar);
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onSetTitleTextColor(String argHtmlColorCode) {
+            //"#ffffff"
+            spinalToolBar.setTitleTextColor(Color.parseColor(argHtmlColorCode));
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onSetTitleText(String argTitle) {
+            if (spinalToolBar != null) {
+                spinalToolBar.setTitle(argTitle);
+            } else {
+                ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
+                if (actionBar != null) {
+                    actionBar.setTitle(argTitle);
+                }
+            }
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onSetTitleText(String argTitle, String argSubTitle) {
+            if (spinalToolBar != null) {
+                spinalToolBar.setTitle(argTitle);
+                spinalToolBar.setSubtitle(argSubTitle);
+            } else {
+                ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
+                if (actionBar != null) {
+                    actionBar.setTitle(argTitle);
+                    actionBar.setSubtitle(argSubTitle);
+                }
+            }
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onShowHomeButton() {
+            ((AppCompatActivity) activity).getSupportActionBar().setHomeButtonEnabled(true);
+            ((AppCompatActivity) activity).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onSetBackgroundColor(String argHtmlColorCode) {
+            //"#80000000"
+            ((AppCompatActivity) activity).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(argHtmlColorCode)));
+            return this;
+        }
+
+        public OnSetupSpinalToolBar onSetElevation(int argElevation) {
+            //0
+            ((AppCompatActivity) activity).getSupportActionBar().setElevation(argElevation);
+            return this;
+        }
+    }
+    //|------------------------------------------------------------|
+
+    public class OnSetupSpinalNavDrawer {
+
+        //|------------------------------------------------------------|
+        /*public OnSetupSpinalNavDrawer onConfigureToolBar() {
+            //|------------------------------------------------------------ |
+            return this;
+        }*/
+
+        public OnSetupSpinalNavDrawer onConfigureDrawer() {
+            //|------------------------------------------------------------|
+            actionBarDrawerToggle = new ActionBarDrawerToggle(activity, spinalDrawerLayout, com.rz.spinalnavdrawer.R.string.spinal_nav_drawer_open, com.rz.spinalnavdrawer.R.string.spinal_nav_drawer_close) {
+                /* Called when drawer is closed */
+                public void onDrawerClosed(View view) {
+                    //Put your code here
+                    activity.invalidateOptionsMenu();
+                }
+
+                /* Called when a drawer is opened */
+                public void onDrawerOpened(View drawerView) {
+                    //Put your code here
+                    activity.invalidateOptionsMenu();
+                }
+            };
+            /*sysToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    sysDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            });*/
+            actionBarDrawerToggle.syncState();
+            spinalDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+            //|------------------------------------------------------------|
+            return this;
+        }
+
+        public void onPostCreate() {
+            actionBarDrawerToggle.syncState();
+        }
+
+        public boolean onOptionsItemSelected(MenuItem argMenuItem) {
+            if (actionBarDrawerToggle.onOptionsItemSelected(argMenuItem)) {
+                return true;
+            }
         /*switch (argMenuItem.getItemId()) {
             case android.R.id.home:
                 sysDrawerLayout.openDrawer(GravityCompat.START);
@@ -142,18 +215,37 @@ public class SpinalNavDrawer {
             default:
                 return super.onOptionsItemSelected(argMenuItem);
         }*/
-        return false;
+            return false;
+        }
+
+        public void onConfigurationChanged(Configuration argNewConfig) {
+            actionBarDrawerToggle.onConfigurationChanged(argNewConfig);
+        }
+        /*public void onCreate(int argLayoutResourceId) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rootParentLayout = layoutInflater.inflate(argLayoutResourceId, null, false);
+            sysTvRowTitle = (TextView) rootParentLayout.findViewById(R.id.sysTvRowTitle);
+            sysTvRowTitle.setText("Test_TEXT");
+        }*/
     }
 
-    public void onConfigurationChanged(Configuration argNewConfig) {
-        actionBarDrawerToggle.onConfigurationChanged(argNewConfig);
+    public class OnSetupSpinalDrawerItems {
+        /*public class OnNavigationDrawerClickListener implements ListView.OnItemClickListener {
+            @Override
+            public void onItemClick(AdapterView<?> argParent, View argView, int argPosition, long argId) {
+                //Toast.makeText(context, "NAVIGATION_DRAWER_POSITION: " + argPosition, Toast.LENGTH_LONG).show();
+                sysDrawerList.setItemChecked(argPosition, true);
+                sysDrawerLayout.closeDrawer(sysIdDrawerContainer);
+                for (int i = 0; i < sysDrawerList.getAdapter().getCount(); i++) {
+                    sysDrawerList.getChildAt(i).setBackgroundColor(Color.BLUE);
+                }
+                argParent.getChildAt(argPosition).setBackgroundColor(Color.YELLOW);
+                *//*DisplayView(position);
+                adapterLstDrawer.setSelectedPosition(position - 0, true, colorPrimaryDark);*//*
+                adapterDynamicArray.notifyDataSetChanged();
+            }
+        }*/
     }
-    /*public void onCreate(int argLayoutResourceId) {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rootParentLayout = layoutInflater.inflate(argLayoutResourceId, null, false);
-        sysTvRowTitle = (TextView) rootParentLayout.findViewById(R.id.sysTvRowTitle);
-        sysTvRowTitle.setText("Test_TEXT");
-    }*/
 }
 /*
 https://stackoverflow.com/questions/26788464/how-to-change-color-of-the-back-arrow-in-the-new-material-theme
--- usagesexample/src/main/java/com/rz/usagesexample/ActDashboard.java
+++ usagesexample/src/main/java/com/rz/usagesexample/ActDashboard.java
@@ -1,15 +1,31 @@
 package com.rz.usagesexample;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.rz.sparkeddynamicmodel.AdapterDynamicArray;
+import com.rz.sparkeddynamicmodel.ModelDynamic;
 import com.rz.spinalnavdrawer.SpinalNavDrawer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Rz Rasel on 2017-08-28.
@@ -18,7 +34,18 @@ import com.rz.spinalnavdrawer.SpinalNavDrawer;
 public class ActDashboard extends AppCompatActivity {
     private Activity activity;
     private Context context;
+    //|------------------------------------------------------------|
+    private Toolbar sysToolBar;
+    private DrawerLayout sysDrawerLayout;
+    private RelativeLayout sysIdDrawerContainer;
+    private ListView sysDrawerList;
     private SpinalNavDrawer spinalNavDrawer;
+    private SpinalNavDrawer.OnSetupSpinalToolBar onSetupSpinalToolBar;
+    private SpinalNavDrawer.OnSetupSpinalNavDrawer onSetupSpinalNavDrawer;
+    //|------------------------------------------------------------|
+    private AdapterDynamicArray adapterDynamicArray;
+    private ArrayList<ModelDynamic> modelDrawerItems = new ArrayList<ModelDynamic>();
+    //|------------------------------------------------------------|
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +53,92 @@ public class ActDashboard extends AppCompatActivity {
         setContentView(R.layout.act_dashboard);
         activity = this;
         context = this;
-        Toolbar sysToolBar = (Toolbar) findViewById(R.id.sysToolBar);
-        DrawerLayout sysDrawerLayout = (DrawerLayout) findViewById(R.id.sysDrawerLayout);
+        //|------------------------------------------------------------|
+        for (int count = 0; count < 20; count++) {
+            HashMap<String, Object> mapItems = new HashMap();
+            mapItems.clear();
+            mapItems.put("title", getObjectValue("Title - " + count));
+            mapItems.put("description", getObjectValue("Description - " + count));
+            modelDrawerItems.add(getSetModelData(mapItems));
+        }
+        //|------------------------------------------------------------|
+        sysDrawerList = (ListView) findViewById(R.id.sysDrawerList);
+        sysToolBar = (Toolbar) findViewById(R.id.sysToolBar);
+        sysDrawerLayout = (DrawerLayout) findViewById(R.id.sysDrawerLayout);
+        sysIdDrawerContainer = (RelativeLayout) findViewById(R.id.sysIdDrawerContainer);
+        //|------------------------------------------------------------|
+        adapterDynamicArray = new AdapterDynamicArray(context, R.layout.layout_navigation_drawer_row, modelDrawerItems);
+        ArrayList<AdapterDynamicArray.ModelRowViewHolder> listRowViewFields = null;
+        listRowViewFields = new ArrayList<AdapterDynamicArray.ModelRowViewHolder>();
+        listRowViewFields.add(adapterDynamicArray.onGetSetModelRowViewData(new TextView(context), "sysDrawerTitle", ""));
+        listRowViewFields.add(adapterDynamicArray.onGetSetModelRowViewData(new TextView(context), "sysDrawerDescription", ""));
+        adapterDynamicArray.onSetListRowViewFields(new AdapterDynamicArray.OnFieldListenerHandler() {
+            @Override
+            public void onSetFieldValue(ArrayList<AdapterDynamicArray.ModelRowViewHolder> argListRowViewFields, Object argObject) {
+                ModelDynamic item = (ModelDynamic) argObject;
+                TextView rowField = null;
+                if (argListRowViewFields.size() > 0) {
+                    rowField = (TextView) argListRowViewFields.get(0).getFieldObject();
+                    rowField.setText(item.getMapItems().get("title").toString());
+                    rowField = (TextView) argListRowViewFields.get(1).getFieldObject();
+                    rowField.setText(item.getMapItems().get("description").toString());
+                }
+            }
+        }, listRowViewFields);
+        sysDrawerList.setAdapter(adapterDynamicArray);
+        //|------------------------------------------------------------|
         spinalNavDrawer = new SpinalNavDrawer(activity, context);
-        spinalNavDrawer.onConfigureToolBar(sysToolBar)
-                .onConfigureDrawer(sysDrawerLayout);
+        onSetupSpinalNavDrawer = spinalNavDrawer.new OnSetupSpinalNavDrawer();
+        onSetupSpinalToolBar = spinalNavDrawer.new OnSetupSpinalToolBar();
+        spinalNavDrawer.setToolBar(sysToolBar)
+                .setDrawerLayout(sysDrawerLayout)
+                .setDrawerContainerLayout(sysIdDrawerContainer)
+                .setDrawerListView(sysDrawerList);
+        onSetupSpinalToolBar.onHideActionBar()
+                .onSetActionBar()
+                .onSetTitleText("Spinal Drawer")
+                .onShowHomeButton()
+                .onSetTitleTextColor("#ffffff")
+                .onSetBackgroundColor("#80000000")
+                .onSetElevation(0);
+        onSetupSpinalNavDrawer.onConfigureDrawer();
+        //|------------------------------------------------------------|
+        sysDrawerList.setOnItemClickListener(new OnNavigationDrawerClickListener());
+        //|------------------------------------------------------------|
     }
+
+    //|------------------------------------------------------------|
+    private class OnNavigationDrawerClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> argParent, View argView, int argPosition, long argId) {
+            Toast.makeText(context, "NAVIGATION_DRAWER_POSITION: " + argPosition, Toast.LENGTH_LONG).show();
+            sysDrawerList.setItemChecked(argPosition, true);
+            sysDrawerLayout.closeDrawer(sysIdDrawerContainer);
+            //for (int pos = 0; pos < adapterDynamicArray.getCount(); pos++) {
+            for (int pos = 0; pos <= sysDrawerList.getLastVisiblePosition() - sysDrawerList.getFirstVisiblePosition(); pos++) {
+                //sysDrawerList.getChildAt(i).setBackgroundColor(Color.BLUE);
+                View parentView = null;
+                //View parentView = adapterDynamicArray.getView(pos, null, sysDrawerList);
+                //sysDrawerList.getItemAtPosition(pos);
+                parentView = sysDrawerList.getChildAt(pos);
+                parentView.setBackgroundColor(Color.BLUE);
+                //parentView.getCh
+                //adapterDynamicArray.notifyDataSetChanged();
+            }
+            argParent.getChildAt(argPosition).setBackgroundColor(Color.YELLOW);
+            /*DisplayView(position);
+            adapterLstDrawer.setSelectedPosition(position - 0, true, colorPrimaryDark);*/
+            adapterDynamicArray.notifyDataSetChanged();
+        }
+    }
+
 
     //|------------------------------------------------------------|
     @Override
     public boolean onOptionsItemSelected(MenuItem argMenuItem) {
         switch (argMenuItem.getItemId()) {
             case android.R.id.home:
-                spinalNavDrawer.onOptionsItemSelected(argMenuItem);
+                onSetupSpinalNavDrawer.onOptionsItemSelected(argMenuItem);
                 return true;
             default:
                 return super.onOptionsItemSelected(argMenuItem);
@@ -51,7 +151,70 @@ public class ActDashboard extends AppCompatActivity {
     @Override
     public void onConfigurationChanged(Configuration argNewConfig) {
         super.onConfigurationChanged(argNewConfig);
-        //spinalNavDrawer.onConfigurationChanged(argNewConfig);
+        onSetupSpinalNavDrawer.onConfigurationChanged(argNewConfig);
     }
-//|------------------------------------------------------------|
+
+    //|------------------------------------------------------------|
+    //private ModelDynamic getSetModelData(String argKey, String argValue) {
+    private ModelDynamic getSetModelData(HashMap<String, Object> argMapItems) {
+        /*HashMap<String, Object> mapItems = new HashMap();
+        mapItems.clear();
+        mapItems.put(argKey, getObjectValue(argValue));*/
+        return new ModelDynamic(argMapItems);
+    }
+
+    private Object getObjectValue(String argValue) {
+        return argValue;
+    }
+
+    //|------------------------------------------------------------|
+    //|------------------------------------------------------------|
+    private void DisplayView(int position) {
+        //FragmentManager fragmentManager = getFragmentManager();
+        //fragmentEventListener;
+        String toolbarTitle = "CMDSS";
+        Fragment fragment = null;
+        Bundle bundle = null;
+        //fragment = new FragTest();
+        if (fragment != null) {
+            //FragmentManager fragmentManager = getFragmentManager();
+            //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            //fragmentTransaction.replace(R.id.frame_container, Fragment.instantiate(ActSplash.this, fragment));
+            if (bundle == null) {
+                bundle = new Bundle();
+            }
+            //bundle.putSerializable(APPConstants.SESSION.KEY, userSession);
+            fragment.setArguments(bundle);
+            fragmentTransaction.replace(R.id.sysFrameContainer, fragment);
+            fragmentTransaction.commit();
+
+            // update selected item and title, then close the drawer
+            sysDrawerList.setItemChecked(position, true);
+            sysDrawerList.setSelection(position);
+            setTitle(toolbarTitle);
+        } else {
+            // error in creating fragment
+            Log.e("Dashboard", "Error in creating fragment");
+        }
+        sysDrawerList.setItemChecked(position, true);
+        sysDrawerLayout.closeDrawer(sysIdDrawerContainer);
+    }
 }
+/*
+***http://android.amberfog.com/?p=296
+https://stackoverflow.com/questions/32352444/androidlayout-height-attr-actionbarsize-is-not-working-with-supportdesign2
+https://stackoverflow.com/questions/23863218/how-to-get-parent-view-of-checkbox-in-listview-items
+
+public View getViewByPosition(int pos, ListView listView) {
+final int firstListItemPosition = listView.getFirstVisiblePosition();
+final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+    return listView.getAdapter().getView(pos, null, listView);
+} else {
+    final int childIndex = pos - firstListItemPosition;
+    return listView.getChildAt(childIndex);
+}}
+*/
